0f8a47465333720a769debd157b652dd5a8d3f1b
albanie <albanie@robots.ox.ac.uk>
2022-12-13 10:34:04 +0000

    Include modifications

--- visualisations/script.js
+++ visualisations/script.js
@@ -1,3 +1,6 @@
+// Note: this code is modifed from https://bl.ocks.org/lsbardel/83ea5911884ae2773991eda6c11856b2
+// which is released under the 3-Clause BSD License.
+
 (function () {
 
     // Make things really fast
